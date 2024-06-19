9c3eae31a33e866a28d61a4c8ffd48e74ff0d9a6
DinahFitiavana <hei.dinah.3@gmail.com>
2024-06-19 09:40:46 +0300

    first commit

---
+++ src/main/java/com/example/pointage_et_salaire/PointageEtSalaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.pointage_et_salaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointageEtSalaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointageEtSalaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pointage_et_salaire/PointageEtSalaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointage_et_salaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointageEtSalaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
